3d8cc9c5b1852df5180c23e56ebf0c2fb5376a40
Leandro Junior <leandrojlhawrysz@usp.br>
2023-07-18 19:32:12 -0300

    Initial Commit

---
+++ MyTrainer/MyTrainerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTrainerApp.swift
+//  MyTrainer
+//
+//  Created by Junior, Leandro on 18/07/23.
+//
+
+import SwiftUI
+
+@main
+struct MyTrainerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
